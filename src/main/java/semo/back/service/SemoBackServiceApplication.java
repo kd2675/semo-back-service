d49416b5096abfe5c459b79eb159a88210492141
김도영 <zz01064842675@gmail.com>
2026-02-02 16:02:03 +0900

    feat: Spring Boot 기반 semo-back-service 초기 설정 추가

---
+++ src/main/java/semo/back/service/SemoBackServiceApplication.java
@@ -0,0 +1,13 @@
+package semo.back.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemoBackServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemoBackServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/semo/back/service/SemoBackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package semo.back.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemoBackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
